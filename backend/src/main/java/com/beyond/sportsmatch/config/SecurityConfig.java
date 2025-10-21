7f69c66458b168cb868f6c40f15e6cef796c53d3
LeeJingu01 <sbeom11@naver.com>
2025-10-21 10:09:50 +0900

    CI/CD 테스트

--- backend/src/main/java/com/beyond/sportsmatch/config/SecurityConfig.java
+++ backend/src/main/java/com/beyond/sportsmatch/config/SecurityConfig.java
@@ -52,6 +52,7 @@ public class SecurityConfig {
 
 
     // Spring Security 필터 체인
+    // 테스트
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
