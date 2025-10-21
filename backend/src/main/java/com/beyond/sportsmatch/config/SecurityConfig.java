f9bbff0552bbe77c247c76c63d9964720e35bb89
LeeJingu01 <sbeom11@naver.com>
2025-10-21 11:48:24 +0900

    CI/CD Test

--- backend/src/main/java/com/beyond/sportsmatch/config/SecurityConfig.java
+++ backend/src/main/java/com/beyond/sportsmatch/config/SecurityConfig.java
@@ -46,6 +46,7 @@ public class SecurityConfig {
 
     // AuthenticationManager (로그인 시 필요)
     // 테스트
+    // 테스트 2
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
         return configuration.getAuthenticationManager();
